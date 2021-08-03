c9778cc9f5c6d327b6f49aa319dbc0df99618e30
aufdj <68519879+aufdj@users.noreply.github.com>
2021-08-03 14:29:02 -0400

    Update fpaq0f.rs

--- fpaq0f.rs
+++ fpaq0f.rs
@@ -82,7 +82,7 @@ impl StateMap {
 
         // updates context_map based on the difference between the predicted and actual bit
         #[allow(overflowing_literals)]
-        let high_23_bit_mask: i32 = 0xfffffe00;
+        let high_23_bit_mask: i32 = 0xFFFFFE00;
         self.context_map[self.context] = self.context_map[self.context].wrapping_add((((bit << 18) - prediction) * self.recipr_table[count] & high_23_bit_mask) as u32);
         
     }
